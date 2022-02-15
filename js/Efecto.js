5a24d7b0a098a8d2d3839bad166bf81f09bafaf5
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-02-15 09:09:37 -0500

    Segundo cambio

--- js/Efecto.js
+++ js/Efecto.js
@@ -1,5 +1,33 @@
-$( document ).ready(function() {
-   $('.search').click(function(){
-       $("#form_input").css({"display":"none")
-   })
+
+var contador = 0;
+$(document).ready(function() {
+
+
+    $('.search').click(function() {
+      if(contador==0){
+        contador = 1
+        console.log('Contador : ',contador)
+          $('#form_input').hide();
+      }else{
+        contador = 0 
+         console.log('Contador : ',contador)
+         $('#form_input').show();
+      }
+    
+            
+    })
+
+
+     /* $('.search').click(function() {
+          
+               $('#form_input').show();
+            
+    })*/
+
+
+
+
+
+
+
 });
